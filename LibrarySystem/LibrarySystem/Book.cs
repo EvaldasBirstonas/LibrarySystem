636573d69ce2e695ecb5b8f490a7fc772f18dc22
Pakaz1 <61023046+Pakaz1@users.noreply.github.com>
2021-06-08 18:08:04 +0300

    Added book class and adding/getting books

---
+++ LibrarySystem/LibrarySystem/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public DateTime Publication_date { get; set; }
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// Book constructor
+        /// </summary>
+        /// <param name="name">Book name</param>
+        /// <param name="author">Author name</param>
+        /// <param name="category">Categoty of the book</param>
+        /// <param name="language">Language of the book</param>
+        /// <param name="publication_date">Book publication date</param>
+        /// <param name="isbn">International Standard Book Number</param>
+        public Book(string name, string author, string category, string language, DateTime publication_date, string isbn)
+        {
+            Name = name;
+            Author = author;
+            Category = category;
+            Language = language;
+            Publication_date = publication_date;
+            ISBN = isbn;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + " " + Author + " " + Category + " " + Language + " " + Publication_date + " " + ISBN;
+        }
+    }
+}
--- LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/Program.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace LibrarySystem
 {
     class Program
     {
+        static public List<Book> bookCollection = new List<Book>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            bookCollection = GetAllBooks();
+            foreach (Book book in bookCollection)
+            {
+                Console.WriteLine(book.ToString());
+            }
+            //Book newBook = new Book("A", "B", "C", "D", DateTime.Now, "TRIETOJ415");
+            //Console.WriteLine(newBook.GetISBN());
+            //bookCollection.Add(newBook);
+            //bookCollection.Add(newBook);
+            //AddBook(newBook);
+        }
+
+        static Boolean AddBook(Book book)
+        {
+            try
+            {
+
+                var obj = JsonConvert.SerializeObject(bookCollection, Formatting.Indented);
+                string workingDirectory = Environment.CurrentDirectory;
+                string bookDirectory = System.IO.Directory.GetParent(workingDirectory).Parent.Parent.FullName + "\\Books_json\\";
+                //Console.WriteLine(System.IO.Directory.GetParent(workingDirectory).Parent.Parent.FullName);
+                string fileName = "books.json";
+                Console.WriteLine(fileName);
+                File.WriteAllText(bookDirectory + fileName, obj);
+                Console.WriteLine(obj.ToString());
+                
+                /*
+                string fileName = book.GetISBN() + ".json";
+                string jsonString = JsonSerializer.Serialize(book);
+                File.WriteAllText("./Books_json/" + fileName, jsonString);
+                Console.WriteLine(jsonString);
+                Console.WriteLine(File.ReadAllText(fileName));
+                */
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        static List<Book> GetAllBooks()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string bookDirectory = System.IO.Directory.GetParent(workingDirectory).Parent.Parent.FullName + "\\Books_json\\";
+            string fileName = "books.json";
+            return JsonConvert.DeserializeObject<List<Book>>(File.ReadAllText(bookDirectory + fileName));
         }
     }
 }
